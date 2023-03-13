b1389581a6a765fb71f1f605e0832db9d843fc81
AlifianZulfaani <genshinimpactindo70@gmail.com>
2023-03-13 09:53:30 +0700

    update init 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <logos-world />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LogosWorld from "./components/LogosWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LogosWorld,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/LogosWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="logos-wrapper">
+    <h2 class="title-header">Find Best Logos</h2>
+    <div class="item-logos">
+      <img class="logos" alt="adidas logo" src="../assets/adidas-logo.png" />
+    </div>
+    <div class="item-logos">
+      <img class="logos" alt="nike logo" src="../assets/nike-logo.png" />
+    </div>
+    <div class="item-logos">
+      <img class="logos" alt="puma logo" src="../assets/puma-logo.png" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LogosWorld",
+};
+</script>
+
+<style scoped>
+.title-header {
+  margin-bottom: 80px;
+}
+
+.logos-wrapper {
+  width: 70%;
+  overflow: hidden;
+  margin: auto;
+  background: navajowhite;
+}
+
+.item-logos {
+  float: left;
+  margin-bottom: 20px;
+  margin-right: 20px;
+  width: 150px;
+  height: 150px;
+  background: yellowgreen;
+}
+
+.item-logos .logos {
+  width: 150px;
+  height: 150px;
+}
+</style>
