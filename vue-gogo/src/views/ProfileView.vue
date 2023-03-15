728e675312032af2d82c79e03564653f2cf286c8
AlifianZulfaani <genshinimpactindo70@gmail.com>
2023-03-15 22:03:03 +0700

    Update Profile Vue

--- vue-gogo/src/views/ProfileView.vue
+++ vue-gogo/src/views/ProfileView.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="profile">
-    <h1>This is a profile page</h1>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <img src="../assets/alep.png" class="mr-3 user-pic" />
+            </div>
+            <div class="flex-grow-1 ms-3 text-start">
+              <h5>Alepp - KokoShiWo</h5>
+              <p>
+                This is some content from a media component. You can replace
+                this with any content and adjust it as needed.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
 
 export default {
   name: "ProfileView",
-  components: {
-    // HelloWorld
-  },
+  components: {},
 };
 </script>
+
+<style scoped>
+.user-pic {
+  width: 90px;
+  height: 90px;
+  border-radius: 200%;
+}
+</style>
+// style scope hanya berdampak pada file tersebut
